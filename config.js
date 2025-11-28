783e302f4fc4217c0c8c1317e15c3e871cedb74c
cjy205205 <cjy205205@chunjae.co.kr>
2025-11-28 10:31:40 +0900

    [feat] 제목수정 테스트

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const users = [
   {
     id: 0, // default author
     username: "licat",
-    company: "weniv",
+    company: "tech",
     position: "CEO",
     img: "img/user/profile-licat.png",
   },
